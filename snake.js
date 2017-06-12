51212df2a40dbdd59afa8cbbb121efd831c95d91
mrmatt1877 <mrmatt1877>
2017-06-12 12:22:56 -0400

    added snake length and no barriers

--- snake.js
+++ snake.js
@@ -1,15 +1,7 @@
 
-window.onload = function(){
-  var canvas = document.getElementById("myCanvas");
-  window.ctx = canvas.getContext("2d");
+var canvas;
+var ctx;
 
-  canvas.width = Math.round(window.innerWidth / 20) * 20 - 100;
-  canvas.height = Math.round(window.innerHeight / 20) * 20 - 100;
-  ctx.fillStyle = "#2386ab";
-  ctx.fillRect(0,0,canvas.width,canvas.height);
-  snakeStart();
-  setInterval(game,1000/15);
-}
 
 var width = Math.round(window.innerWidth / 20) * 20 - 100;
 var height = Math.round(window.innerHeight / 20) * 20 - 100;
@@ -17,13 +9,29 @@ var foodReady = false;
 var snakex = 240;
 var snakey = 400;
 var snake = [[snakex,snakey]];
+var snakeTail = 1;
 var food;
 var preFood;
 
 
-function game(){
+window.onload = function(){
+  canvas = document.getElementById("myCanvas");
+  ctx = canvas.getContext("2d");
+  canvas.width = Math.round(window.innerWidth / 20) * 20 - 100;
+  canvas.height = Math.round(window.innerHeight / 20) * 20 - 100;
 
-//placing the food
+  setInterval(game,1000/15);
+}
+
+
+function game(){
+  //draw the board and then the snake each time
+  ctx.fillStyle = "#2386ab";
+  ctx.fillRect(0,0,canvas.width,canvas.height);
+  draw();
+
+
+//generating random food placement
   function makeFood(){
     var foodx = Math.round(width * Math.random() / 20) * 20 - 20;
     var foody = Math.round(height * Math.random() / 20) * 20 - 20;
@@ -34,72 +42,112 @@ function game(){
       foody = 0
     };
 
-    ctx.fillStyle="#13dd13";
-    food = ctx.fillRect(foodx,foody,20,20);
-    console.log(foodx, foody);
     return [foodx, foody]
   };
 
 //checking if food is present and places new food if false
-  console.log("food is here: " + foodReady);
+  //console.log("food is here: " + foodReady, preFood );
   if(foodReady == false){
     preFood = makeFood();
     foodReady = true;
   };
 
+  ctx.fillStyle="#13dd13";
+  food = ctx.fillRect(preFood[0],preFood[1],20,20);
   //placeholder for when player gets food removes food and makes food presence false
-  if(snakex == preFood[0] && snakey == preFood[1]){
-    foodReady = false
-  };
 
 
 }
-
+function snakeGrow(){
+  if(snakex == preFood[0] && snakey == preFood[1]){
+    foodReady = false;
+    snake.push([])
+    snakeTail++;
+  };
+}
 
 //maping snake start
-function snakeStart(){
+/*function snakeStart(){
   console.log(snake);
   for (var i = 1; i < 1; i++) {
     snake.push([(snake[snake.length-1][0] + 20), snakey]);
     ctx.fillStyle="#23cce5";
     ctx.fillRect(snake[i][0],snake[i][1],20,20);
   }
-};
+};*/
 
 //drawing the snake
 function draw(){
-  snake.push([(snakex), snakey]);
-  ctx.fillStyle = "#2386ab";
-  ctx.fillRect(snake[snake.length-2][0],snake[snake.length-2][1],20,20)
+
+  if(snakex > width) {
+    snakex = 0;
+  }
+  if(snakex < 0){
+    snakex = width-20;
+  }
+  if(snakey > height) {
+    snakey = 0;
+  }
+  if(snakey < 0){
+    snakey = height-20;
+  }
+
+  snake[0][0] = snakex;
+  snake[0][1] = snakey;
+  //ctx.fillStyle = "#2386ab";
+  //ctx.fillRect(snake[snake.length-2][0],snake[snake.length-2][1],20,20)
   ctx.fillStyle="#23cce5";
-  ctx.fillRect(snake[snake.length-1][0],snake[snake.length-1][1],20,20);
-  console.log(snakex,snakey, snake[1][0]);
+  ctx.fillRect(snake[0][0],snake[0][1],20,20);
+
+  if(snakeTail>1){
+    for (var i = 1; i < snakeTail; i++) {
+      ctx.fillStyle="#23cce5";
+      ctx.fillRect(snake[i][0],snake[i][1],20,20);
+    }
+  }
 }
 
 
+//keeping track of previous moves
+function snakeMemory(){
+  if(snakeTail>1){
+    for (var i = 1; i < snakeTail; i++) {
+      snake[snake.length - i][0] = snake[(snake.length-i)-1][0];
+      snake[snake.length - i][1] = snake[(snake.length-i)-1][1];
+    }
+  }
+}
 //listening for button presses and then drawing the new snake
 document.addEventListener("keydown",keyPush);
 function keyPush(evt) {
   switch(evt.keyCode) {
     case 37:
+      snakeMemory();
       snakex = snakex-20;
       console.log('left');
-      draw();
+      snakeGrow();
+      console.log(snake);
       break;
     case 38:
+      snakeMemory();
       snakey = snakey-20;
       console.log('up');
-      draw();
+      snakeGrow();
+      console.log(snake);
       break;
     case 39:
+      snakeMemory();
       snakex = snakex +20
       console.log('right');
-      draw();
+      snakeGrow();
+      console.log(snake);
       break;
     case 40:
+      snakeMemory();
       snakey = snakey +20;
       console.log('down');
-      draw();
+      snakeGrow();
+      console.log(snake);
       break;
   }
 }
